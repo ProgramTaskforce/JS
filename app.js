d1a21b13845bc8a7a89ff15130031a440ad23e2e
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-09-25 00:18:29 +0200

    erste if versuche mit kalkulationen

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let loggedIn = true
-let subscribed = true
+let cash = 0
+let price = 40
+let calculation = (cash - price)
+let missingCalculation = (price - cash)
 
+if (cash > price) {
+    console.log ("You paid extra, here is " + calculation + "$ change.")
 
-
-
-if (loggedIn && subscribed === true) {
-    console.log ("show the video")
 }
 
-
-else if(loggedIn === false) {
-    console.log ("log into your account")
+else if (cash === price) {
+    console.log ("You paid the right amount, have a nice day.")
 }
 
-else if(subscribed === false) {
-    console.log("subscribe to pro")
+else {
+    console.log ("You dont have enough money, you still owe me " + calculation * -1 + "$.")
 }
+// bei meinem ersten versuch, hatte ich statt calculation * -1 , das missingCalculation eingesetzt
