56423270416418a5b71cc4624525150aacfc35b9
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-12 13:38:21 +0100

    truthy falsy

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-let cash = 0
-let price = 40
-let calculation = (cash - price)
-let missingCalculation = (price - cash)
+let cash = 50;
+let price = 40;
+let isStoreOpen = true;
 
-if (cash > price) {
-    console.log ("You paid extra, here is " + calculation + "$ change.")
-
+if (cash >= price && isStoreOpen === true) {
+  console.log("Print the recipt");
 }
+// best practise wäre: 
+// if (cash >= price && isStoreOpen) {
+//  .....
+// }
+// also ohne das === true, weil es automatisch nach true schaut
+// mit ! ( ausrufezeichen) vor dem isStoreOpen , schaut er automatisch nach 
+// dem Gegenteil , also false
 
-else if (cash === price) {
-    console.log ("You paid the right amount, have a nice day.")
-}
 
-else {
-    console.log ("You dont have enough money, you still owe me " + calculation * -1 + "$.")
-}
-// bei meinem ersten versuch, hatte ich statt calculation * -1 , das missingCalculation eingesetzt
+
+
+
+// let cash = 30
+// let price = 50
+// let payment = cash - price
+// 
+// if (cash > price) {
+//   console.log("you paid extra - here is " + payment + "$ change.")
+// }
+// 
+// else if (cash < price) {
+//   console.log("Thats not enough money, you still owe " + payment * -1 + "$.")
+// }
+// 
+// else {
+//   console.log("You paid the exact amount, have a nice day.")
+// }
