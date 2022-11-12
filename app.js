02f58cbacb4b6448fffe16991ca4e60b9ef3df9f
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-12 13:51:06 +0100

    ternay operators

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
-let cash = 50;
-let price = 40;
-let isStoreOpen = true;
-
-if (cash >= price && isStoreOpen === true) {
-  console.log("Print the recipt");
-}
-// best practise wäre: 
-// if (cash >= price && isStoreOpen) {
-//  .....
-// }
-// also ohne das === true, weil es automatisch nach true schaut
-// mit ! ( ausrufezeichen) vor dem isStoreOpen , schaut er automatisch nach 
-// dem Gegenteil , also false
-
-
-
-
-
-// let cash = 30
-// let price = 50
-// let payment = cash - price
+// let hot = false
 // 
-// if (cash > price) {
-//   console.log("you paid extra - here is " + payment + "$ change.")
-// }
+// hot ? console.log("weather is hot outside") : console.log("weather is cold")
+
+// let subscribed = true 
+// let loggedIn = true 
 // 
-// else if (cash < price) {
-//   console.log("Thats not enough money, you still owe " + payment * -1 + "$.")
-// }
-// 
-// else {
-//   console.log("You paid the exact amount, have a nice day.")
-// }
+// let str = subscribed && loggedIn ? " show the video" : "hide video"
+// console.log(str);
+
+let cash = 50
+let price = 40
+let isStoreOpen = true
+
+let str = cash >= price && isStoreOpen ? "print the recipt" : "dont print the recipt"
+console.log(str)
