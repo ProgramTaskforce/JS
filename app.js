07a339d47d4a09fa3182405a5791195c8041a70c
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-13 22:26:27 +0100

    loop counting first exercise

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 // for (let i = 0; i < 3; i = i +1) {
 //   console.log(i);
 // }
-
-for (let i = 1; i <= 20; i++) {
   
-if (i % 3 === 0 && i % 5 === 0) {
-  console.log("Frontend Simplified")
-}
-  
-else if (i % 3 === 0) {
-  console.log("Frontend");
-}
 
-else if (i % 5 === 0) {
-  console.log("Simplified")
-}
 
-else {
-  console.log(i)
-}
+// for (let i = 1; i <= 20; i++) {
+//   
+// if (i % 3 === 0 && i % 5 === 0) {
+//   console.log("Frontend Simplified")
+    
+// }
+//   
+// else if (i % 3 === 0) {
+//   console.log("Frontend");
+// }
+// 
+// else if (i % 5 === 0) {
+//   console.log("Simplified")
+// }
+// 
+// else {
+//   console.log(i)
+// }
+// 
+// }
 
-}
+
+ for (let i = 1; i <= 20; i++) {
+ 
+   if(i % 3 === 0 && i % 5 === 0) {
+     console.log(`${i} -> Frontend Simplified`)
+   }
+ 
+   else if(i % 3 === 0) {
+     console.log(`${i} -> Frontend`)
+   }
+
+   else if(i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+   }
+
+   else {
+    console.log(`${i} -> ${i} `)
+   }
+ }
