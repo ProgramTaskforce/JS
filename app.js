b24bac336542a5adb27fd724010f7e8785ee28b5
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-14 20:22:13 +0100

    FUNCTIONS first example and DEFINITION

--- app.js
+++ app.js
@@ -1,52 +1,14 @@
-// for (let i = 0; i < 3; i = i +1) {
-//   console.log(i);
-// }
-  
+// console.log("welcome to FES, David")
+// console.log("welcome to FES, Zen")
+// console.log("welcome to FES, Mitri")
 
+// DRY - Dont repeat yourself
 
-// for (let i = 1; i <= 20; i++) {
-//   
-// if (i % 3 === 0 && i % 5 === 0) {
-//   console.log("Frontend Simplified")
-    
-// }
-//   
-// else if (i % 3 === 0) {
-//   console.log("Frontend");
-// }
-// 
-// else if (i % 5 === 0) {
-//   console.log("Simplified")
-// }
-// 
-// else {
-//   console.log(i)
-// }
-// 
-// }
-
-
-// for (let i = 1; i <= 20; i++) {
-// 
-//   if(i % 3 === 0 && i % 5 === 0) {
-//     console.log(`${i} -> Frontend Simplified`)
-//   }
-// 
-//   else if(i % 3 === 0) {
-//     console.log(`${i} -> Frontend`)
-//   }
-//
-//   else if(i % 5 === 0) {
-//    console.log(`${i} -> Simplified`)
-//   }
-//
-//   else {
-//    console.log(`${i} -> ${i} `)
-//   }
-// }
-
-const str = "Frontend Simplified"
-
-for (let i = 0; i < str.length; i++) {
-  console.log(str[i]);
+// function Definition
+function welcomePersonToFES(firstName, lastName) {        // name is the PARAMETER in this example, in this function DEFINITION  
+  console.log(`Welcome to Frontend Simplified, ${firstName} ${lastName}`)   // nothing is printing out of the console, because functions dont execute by them selfs
 }
+
+// Call the function
+welcomePersonToFES("David", "Bragg");
+welcomePersonToFES("Semjon", "Task");
