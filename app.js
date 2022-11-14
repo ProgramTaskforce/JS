871b058621b54e4cacae6b19deede17bea770605
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-14 20:52:42 +0100

    first Task Function convert F to C

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-function sumOfTwoNumbers(num1, num2) {             // num 1 and num2 here is called PARAMETER
-  return num1 + num2                    
+// function sumOfTwoNumbers(num1, num2) {             // num 1 and num2 here is called PARAMETER
+//   return num1 + num2                    
+// }
+// 
+// console.log(sumOfTwoNumbers(10, 10));            // 10 and 10 here is called ARGUMENT
+
+/** Create a function that converts Celsius to Fahrenheit
+ * 
+ * Celsius to Fahrenheit formula:
+ * F = C x 1.8 + 32
+ * 
+ * @example
+ * convertCelsiusToFahrenheit(0) -> 32
+ * convertCelsiusToFahrenheit(10) -> 50
+ * convertCelsiusToFahrenheit(30) -> 86
+ */
+
+function convertCelsiusToFahrenheit(celsius) {
+  return celsius * 1.8 + 32
 }
 
-console.log(sumOfTwoNumbers(10, 10));            // 10 and 10 here is called ARGUMENT
+console.log(convertCelsiusToFahrenheit(0));
+console.log(convertCelsiusToFahrenheit(10));
+console.log(convertCelsiusToFahrenheit(30));
