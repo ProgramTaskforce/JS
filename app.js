09d2a4e9b42233e64b150c69e7765ad9903a4793
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-14 20:28:48 +0100

    Functions RETURN

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// console.log("welcome to FES, David")
-// console.log("welcome to FES, Zen")
-// console.log("welcome to FES, Mitri")
-
-// DRY - Dont repeat yourself
-
-// function Definition
-function welcomePersonToFES(firstName, lastName) {        // name is the PARAMETER in this example, in this function DEFINITION  
-  console.log(`Welcome to Frontend Simplified, ${firstName} ${lastName}`)   // nothing is printing out of the console, because functions dont execute by them selfs
+function fn() {
+  return 5                      // return function blocks every line of code that is following after
+  console.log(`my function`)
 }
 
-// Call the function
-welcomePersonToFES("David", "Bragg");
-welcomePersonToFES("Semjon", "Task");
+fn();
