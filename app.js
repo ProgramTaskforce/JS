0260c54b48eff358718fa843c6204916712e762d
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-15 21:26:54 +0100

    noch schlankerer code ffür FilterArray

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 let arr = [20, 30, 40, 50, 100]
 
-let newArr = arr.filter((element) => {
-  console.log(element)                             // the array loop, prints every element ONE BY ONE, 
-  if (element < 50){
-    return true;
-  }
-})
-
-console.log(newArr)                                 //  loop returns 3 times, printing 20,30,40 because of the else statement
-
-// SCHLANKER machern
-
 
 let newArr = arr.filter(element => {                // wenn nur 1 element, können die doppelten klammern weg (()) -> ()
-  return (element < 50){                                 // remove console.log, its doing nothing, delete return true and if, and put the reutrn instead of if
+  return (element < 50)                             // remove console.log, its doing nothing, delete return true and if, and put the reutrn instead of if
     
-  }
 })
 
 console.log(newArr) 
