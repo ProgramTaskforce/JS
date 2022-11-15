65c79df2a0d7f7ab92b8f365a3062a0c82f2b48d
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-15 21:51:13 +0100

    1. Übung mit Arrays

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-let arr = [20, 30, 40, 50, 100]
+// let arr = [20, 30, 40, 50, 100]
+// 
+// let newArr = arr.filter(element => element < 50)               
+// 
+// console.log(newArr) 
 
-let newArr = arr.filter(element => element < 50)               
 
-console.log(newArr) 
+/** TRAINING EXERCISE
+ * 
+ * Filter out all the "Fail" elements in an array
+ * 
+ * @examples
+ * ["A+", "A", "Fail"] => ["A+", "A"] 
+ * ["Fail", "Fail", "B"] => ["B"]
+ * ["Fail"] => []
+ * 
+ */
+
+// Meine erste Lösung
+
+// let grades = ["A+", "A", "Fail"]
+
+// let arr = ['A+', 'A', 'Fail']
+// 
+// let newArr = arr.filter(element => element !== 'Fail')
+// 
+// console.log(newArr)
+
+// BESSER mit genauer bezeichnung
+
+let grades = ['A+', 'A', 'Fail']
+
+let goodGrades = grades.filter(element => element !== 'Fail')
+
+console.log(goodGrades)
