21612efa615b00a92a2be8dac423573a79168b44
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-16 13:38:56 +0100

    1. Exercise MAP array without using MAP, loop instead

--- app.js
+++ app.js
@@ -3,22 +3,20 @@
  * 
  * Turn each element in an array of dollars into cents
  * 
+ * WITHOUT using MAP method
+ * 
  * @examples
  * 
  * [1, 5, 10, 3] => [100, 500, 1000, 300]
  * [0, 10, 20] =Y [0, 1000, 2000]
  */
 
-// let dollars = [1, 5, 10, 3]
-// 
-// let dollarsToCents = dollars.map((element) =>{
-//   return element * 100
-// })
-// 
-// console.log(dollarsToCents)
-
 let dollars = [1, 5, 10, 3]
 
-let dollarsToCents = dollars.map(element => element * 100)
+let dollarsToCents =[]                             // create new dollars to cents array
 
-console.log(dollarsToCents)
+for (let i = 0; i < dollars.length; i++){          // loop over every element in dollars
+  dollarsToCents.push(dollars[i] * 100)            // convert dollars into cents
+}
+
+console.log(dollarsToCents)                        // console log cents
