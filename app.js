81bc368dd693271035a38260590dc2420d8105ba
ProgramTaskforce <semjon.taskforce@gmail.com>
2022-11-18 11:11:49 +0100

    Objects 4 , complete login, email , password

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-let users = [                                       
-  {                                                 
-  username: 'David',                                
-  email: 'david@frontendsimplified.com',
-  password: 'test123',
-  subscriptionStatus: 'VIP',
-  discordID: 'David Bragg#0001',
-  lessonsCompleted: [0, 1]               
-}, 
-{
-  username: 'Mitri',
-  email: 'mitri@frontendsimplified.com',
-  password: 'mitri123',
-  subscriptionStatus: 'VIP',
-  discordID: 'Mitri#0001',
-  lessonsCompleted: [0, 1, 2, 3]   
-},
+let users = [
+  {
+    username: "David",
+    email: "david@frontendsimplified.com",
+    password: "test123",
+    subscriptionStatus: "VIP",
+    discordID: "David Bragg#0001",
+    lessonsCompleted: [0, 1],
+  },
+  {
+    username: "Mitri",
+    email: "mitri@frontendsimplified.com",
+    password: "mitri123",
+    subscriptionStatus: "VIP",
+    discordID: "Mitri#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+  {
+    username: "Zen",
+    email: "Zen@frontendsimplified.com",
+    password: "zen123",
+    subscriptionStatus: "VIP",
+    discordID: "Zen#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
 ];
 
 function login(email, password) {
-  console.log(email, password);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email) {               // only console.log the user , if there is an email with that 
+      console.log(users[i]);                      // email-name, that we put in "login" on line 44
+      if (users[i].password === password) {        // check if the user, that we CURRENT are on, the passwort
+        console.log("log the user in - the details are correct");    // matches the password
+      }
+      else {
+        console.log('pasword is incorrect - try again')
+      }
+      return;                                     // if the loop finds correct emal, this ends the loop afterwards
+    }                                             // if the email of user belongs to first [0] user in the users storage
+  }                                               // it run once, otherwise X-times, depending on place of the users
+  console.log('could not find an emal that matches')  // if email not found after all loops
 }
 
-login('frontendsimplified', 'test123')
+login("david@frontendsimplified.com", "test123");
